aad7dfe54ccd8bc0e0183d37aab562b8a199a725
Polina <ms.maylin@inbox.ru>
2023-03-02 01:18:31 +0300

    Task3 added

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Нахождение факториала числа
+
+// Итеративно
+
+int FactorialFor(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++) result *= i;
+    return result;
+}
+
+Console.WriteLine(FactorialFor(10)); // 3628800
+
+// Рекурсивно
+
+int FactorialRec(int n)
+{
+    if (n == 1 || n == 0) return 1;
+    else return n * FactorialRec(n - 1);
+}
+Console.WriteLine(FactorialRec(10)); // 3628800
