60eef8c9a1cd111f7b60b1d776d245f016aa1c58
Polina <ms.maylin@inbox.ru>
2023-03-02 01:12:16 +0300

    Task2 added

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Сумма чисел от 1 до n
+
+// Итеративный способ:
+
+int SumFor(int n)
+{
+    int result = 0;
+    for (int i = 1; i <= n; i++) result += i;
+    return result;
+}
+Console.WriteLine(SumFor(10)); // 55
+
+// Использование рекурсии:
+
+int SumRec(int n)
+{
+    if (n == 1) return 1;
+    else return n + SumRec(n - 1);
+}
+Console.WriteLine(SumRec(10)); // 55
